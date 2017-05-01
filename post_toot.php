6f43f191c76e6a5fd4e43bd60035e48783eea227
mu-ko <mu-ko@pixiv.com>
2017-05-01 15:51:17 +0900

    画像ファイルの名前が被らないように変更

--- post_toot.php
+++ post_toot.php
@@ -8,8 +8,10 @@ redirectToLoginPageIfNotLoggedIn();
 
 $toot_text = $_POST['text'];
 
+$newfilename = date("YmdHis")."-".$_SESSION['user_id']."-" .$_FILES['image']['name'];
+
 $uploaddir = getcwd().'/uploaded_image/';
-$uploadfile = $uploaddir . basename($_FILES['image']['name']);
+$uploadfile = $uploaddir . $newfilename;
 
 move_uploaded_file($_FILES['image']['tmp_name'], $uploadfile);
 
