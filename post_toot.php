f27f38b1cce79df08b9016d465fcdae5b03e8ff2
mu-ko <mu-ko@pixiv.com>
2017-05-01 12:34:40 +0900

    tootを作る（保存、表示）

--- post_toot.php
+++ post_toot.php
@@ -5,4 +5,18 @@ require_once 'functions.php';
 session_start();
 redirectToLoginPageIfNotLoggedIn();
 
+
+$toot_text = $_POST['text'];
+
+$database = getDatabase();
+$created_at = date('Y-m-d H:i:s');
+$sql="INSERT INTO `toot`(`user_id`, `text`, `image_file_name`,`created_at`) VALUES ({$_SESSION['user_id']},'{$toot_text}','','{$created_at}')";
+//var_dump($sql);
+$database->query($sql);
+// $user = $database->query("
+//     SELECT *
+//     FROM `user`
+//     WHERE `login_name` = '{$login_name}'
+// ")->fetch(PDO::FETCH_ASSOC);
+
 header('Location: /');
