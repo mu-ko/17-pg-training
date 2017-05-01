acbba01542c16e5f410ba0ae20433fcebc282008
mu-ko <mu-ko@pixiv.com>
2017-05-01 17:42:19 +0900

    画像なしのときに透子できないように変更

--- post_toot.php
+++ post_toot.php
@@ -8,14 +8,20 @@ redirectToLoginPageIfNotLoggedIn();
 
 $toot_text = $_POST['text'];
 
-$newfilename = date("YmdHis")."-".$_SESSION['user_id']."-" .$_SESSION['user_login_name'];
-$ext = pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
+if($_FILES['image']['name']!=''){
+  $newfilename = date("YmdHis")."-".$_SESSION['user_id']."-" .$_SESSION['user_login_name'];
+  $ext = pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
+
 
 $uploaddir = getcwd().'/uploaded_image/';
 $uploadfile = $uploaddir . $newfilename . ".".$ext;
 $uploadfile_name =  $newfilename . ".".$ext;
 
 move_uploaded_file($_FILES['image']['tmp_name'], $uploadfile);
+}else{
+  $uploadfile_name = "";
+}
+
 
 $database = getDatabase();
 $created_at = date('Y-m-d H:i:s');
