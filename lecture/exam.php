8152fc5cd1e75b6f342f29d36da3fc28426ac476
mu-ko <mu-ko@pixiv.com>
2017-05-01 10:24:54 +0900

    タイムラインの見た目を作った

---
+++ lecture/exam.php
@@ -0,0 +1,10 @@
+<?php
+  $mathNum = 45;
+  $scienceNum = 35;
+  //$totalNum= 0 ;
+  if( $mathNum >= 30 && $scienceNum >= 30 && ($mathNum + $scienceNum) >= 80){
+      //$totalNum = $mathNum + $scienceNum;
+        print '合格おめでとう';
+  }else{
+        print '合計点が足りません';
+  }
