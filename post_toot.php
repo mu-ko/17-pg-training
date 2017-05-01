04364ebc95c7e01a459dbf85d45c1a7bcec02ce6
mu-ko <mu-ko@pixiv.com>
2017-05-01 16:33:19 +0900

    画像のファイル名を正しくデータベースに保存するように変更

--- post_toot.php
+++ post_toot.php
@@ -17,7 +17,7 @@ move_uploaded_file($_FILES['image']['tmp_name'], $uploadfile);
 
 $database = getDatabase();
 $created_at = date('Y-m-d H:i:s');
-$sql="INSERT INTO `toot`(`user_id`, `text`, `image_file_name`,`created_at`) VALUES ({$_SESSION['user_id']},'{$toot_text}','{$_FILES['image']['name']}','{$created_at}')";
+$sql="INSERT INTO `toot`(`user_id`, `text`, `image_file_name`,`created_at`) VALUES ({$_SESSION['user_id']},'{$toot_text}','{$uploadfile}','{$created_at}')";
 $database->query($sql);
 
 
