ec38d099bb18a45031131887eb0238f29f957ae9
mu-ko <mu-ko@pixiv.com>
2017-05-01 17:18:50 +0900

    画像の名前を自動で変更(日本語対応など)

--- post_toot.php
+++ post_toot.php
@@ -8,16 +8,18 @@ redirectToLoginPageIfNotLoggedIn();
 
 $toot_text = $_POST['text'];
 
-$newfilename = date("YmdHis")."-".$_SESSION['user_id']."-" .$_SESSION['login-name'];
+$newfilename = date("YmdHis")."-".$_SESSION['user_id']."-" .$_SESSION['user_login_name'];
+$ext = pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
 
 $uploaddir = getcwd().'/uploaded_image/';
-$uploadfile = $uploaddir . $newfilename;
+$uploadfile = $uploaddir . $newfilename . ".".$ext;
+$uploadfile_name =  $newfilename . ".".$ext;
 
 move_uploaded_file($_FILES['image']['tmp_name'], $uploadfile);
 
 $database = getDatabase();
 $created_at = date('Y-m-d H:i:s');
-$sql="INSERT INTO `toot`(`user_id`, `text`, `image_file_name`,`created_at`) VALUES ({$_SESSION['user_id']},'{$toot_text}','{$uploadfile}','{$created_at}')";
+$sql="INSERT INTO `toot`(`user_id`, `text`, `image_file_name`,`created_at`) VALUES ({$_SESSION['user_id']},'{$toot_text}','{$uploadfile_name}','{$created_at}')";
 $database->query($sql);
 
 
